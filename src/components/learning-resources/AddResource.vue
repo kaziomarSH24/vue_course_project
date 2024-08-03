3244bbb8c5efb1a3ac47127abf3fa0df2344f9e8
Kazi Omar Faruk <kaziomar144@gmail.com>
2024-08-03 16:10:04 +0600

    Vue Simple Practice Project-Components,props,provider,inject,emit

---
+++ src/components/learning-resources/AddResource.vue
@@ -0,0 +1,89 @@
+<template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>
+        Please check all inputs and make sure you enter at leat a few characters into each input
+        field.
+      </p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="from-control">
+        <label for="title">Title </label>
+        <input name="title" id="title" type="text" ref="titleInput" />
+      </div>
+      <div class="from-control">
+        <label for="description">Description </label>
+        <textarea name="description" id="description" rows="3" ref="desInput"></textarea>
+      </div>
+      <div class="from-control">
+        <label for="link">Link </label>
+        <input name="link" id="link" type="url" ref="linkInput" />
+      </div>
+      <div>
+        <base-button type="submit">Add Button</base-button>
+      </div>
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false
+    }
+  },
+  methods: {
+    submitData() {
+      const enterTitle = this.$refs.titleInput.value
+      const enterDescription = this.$refs.desInput.value
+      const enterLink = this.$refs.linkInput.value
+      if (enterTitle.trim() === '' || enterDescription.trim() === '' || enterLink.trim() === '') {
+        this.inputIsInvalid = true
+        return
+      }
+      this.addResource(enterTitle, enterDescription, enterLink)
+    },
+    confirmError() {
+      this.inputIsInvalid = false
+    }
+  }
+}
+</script>
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+#link {
+  margin-bottom: 8px;
+}
+</style>
